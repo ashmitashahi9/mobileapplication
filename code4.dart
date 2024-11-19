2c14f7f2636cb2c988dc2453f00fcb5b0e66628f
ashmitashahi9 <ashmitashahi67@gmail.com>
2024-11-19 09:23:15 +0545

    second commit

---
+++ code4.dart
@@ -0,0 +1,9 @@
+void main() {
+  int num1 = 15;
+  int num2 = 5;
+
+  print('Addition: ${num1 + num2}');
+  print('Subtraction: ${num1 - num2}');
+  print('Multiplication: ${num1 * num2}');
+  print('Division: ${num1 / num2}');
+}
---
+++ code5.dart
@@ -0,0 +1,11 @@
+void main() {
+  int a = 25, b = 10, c = 15;
+
+  if (a > b && a > c) {
+    print('$a is the largest');
+  } else if (b > c) {
+    print('$b is the largest');
+  } else {
+    print('$c is the largest');
+  }
+}
